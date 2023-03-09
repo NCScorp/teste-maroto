8e7d1217a8a2a6ec2911fc48d0bded3bff3715a1
Nicolas Codonho Scorpelini <nicolasscorpelini@nasajon.com.br>
2023-03-09 17:15:29 -0300

    up

--- utils/sas.py
+++ utils/sas.py
@@ -1,14 +1,8 @@
 import boto3
 
-opts, args = getopt.getopt(sys.argv[1:], 'k:s:f:b:d:', ["key=", "secret=", "file=", "bucket=", "destination="])
+opts, args = getopt.getopt(sys.argv[1:], 'f:b:d:', ["key=", "secret=", "file=", "bucket=", "destination="])
 for opt, arg in opts:
-    if opt in ("-k", "--key"):
-        key = arg
-
-    elif opt in ("-s", "--secret"):
-        secret = arg
-
-    elif opt in ("-f", "--file"):
+    if opt in ("-f", "--file"):
         file = arg
     
     elif opt in ("-b", "--bucket"):
